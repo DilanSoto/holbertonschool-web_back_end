836358a0fff8fd8472196af2dacd7ba689b5707d
Dilan Soto Ortiz <144055652+DilanSoto@users.noreply.github.com>
2024-04-30 22:08:08 -0400

    location error

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" Basic annotations concat """
+
+
+def concat(str1="egg", str2="shell") -> str:
+    """Returns concatenated strings"""
+    return str1 + str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""basic annotations floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor of the float"""
+    return math.floor(n)
